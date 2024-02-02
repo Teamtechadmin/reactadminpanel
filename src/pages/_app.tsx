37b9bda9a63ed13199a529418b7aebf250090c54
akanandhu <akananthu11@gmail.com>
2024-02-02 13:05:46 +0530

    style: padding issues resolved in login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
 import FontWrapper from "@/components/wrappers/FontWrapper";
 import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { Roboto } from 'next/font/google'
+import { ReactNode } from "react";
+import Spinner from '../components/spinner'
 
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
 })
+
+type ExtendedAppProps = AppProps & {
+  Component: NextPage
+}
+
+type GuardProps = {
+  authGuard: boolean
+  guestGuard: boolean
+  children: ReactNode
+}
+
+// const Guard = ({ children, authGuard, guestGuard }: GuardProps) => {
+//   if (guestGuard) {
+//     return <GuestGuard fallback={<Spinner />}>{children}</GuestGuard>
+//   } else if (!guestGuard && !authGuard) {
+//     return <>{children}</>
+//   } else {
+//     return <AuthGuard fallback={<Spinner />}>{children}</AuthGuard>
+//   }
+// }
+
 export default function App({ Component, pageProps }: AppProps) {
+
+  //  const authGuard = Component.authGuard ?? true
+  // const guestGuard = Component.guestGuard ?? false
+
   return (
     <FontWrapper>
       <AuthProvider>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,7 +14,7 @@ import { styled, useTheme } from "@mui/material/styles";
 import FormHelperText from "@mui/material/FormHelperText";
 import InputAdornment from "@mui/material/InputAdornment";
 import MuiFormControlLabel, {
-  FormControlLabelProps,
+    FormControlLabelProps,
 } from "@mui/material/FormControlLabel";
 import * as yup from "yup";
 import { useForm, Controller } from "react-hook-form";
@@ -25,202 +25,201 @@ import BlankLayout from "@/layouts/BlankLayout";
 import { Grid } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import {
-  Visibility,
-  VisibilityOff,
-  VisibilityOffOutlined,
-  VisibilityOutlined,
+    Visibility,
+    VisibilityOff,
+    VisibilityOffOutlined,
+    VisibilityOutlined,
 } from "@mui/icons-material";
 
 const LinkStyled = styled(Link)(({ theme }) => ({
-  fontSize: "0.875rem",
-  textDecoration: "none",
-  fontFamily: "Roboto",
-  color: theme.palette.primary.main,
+    fontSize: "0.875rem",
+    textDecoration: "none",
+    fontFamily: "Roboto",
+    color: theme.palette.primary.main,
 }));
 
 const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(
-  ({ theme }) => ({
-    "& .MuiFormControlLabel-label": {
-      fontSize: "0.875rem",
-      color: theme.palette.text.secondary,
-    },
-  }),
+    ({ theme }) => ({
+        "& .MuiFormControlLabel-label": {
+            fontSize: "0.875rem",
+            color: theme.palette.text.secondary,
+        },
+    }),
 );
 
 const schema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(6).required(),
 });
 
 interface FormData {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 const defaultValues: FormData = {
-  password: "",
-  email: "",
+    password: "",
+    email: "",
 };
 
 const LoginPage = () => {
-  const [rememberMe, setRememberMe] = useState<boolean>(true);
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const router = useRouter();
+    const [rememberMe, setRememberMe] = useState<boolean>(true);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const router = useRouter();
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues,
-    mode: "onBlur",
-    resolver: yupResolver(schema),
-  });
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues,
+        mode: "onBlur",
+        resolver: yupResolver(schema),
+    });
 
-  const onSubmit = () => {
-    router.push("/home");
-  };
+    const onSubmit = () => {
+        router.push("/home");
+    };
 
-  return (
-    <Box
-      display={"flex"}
-      justifyContent={"center"}
-      alignContent={"center"}
-      minHeight={"100%"}
-    >
-      <Box
-        sx={{
-          p: [6, 12],
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          minWidth: '100%'
-        }}
-      >
+    return (
         <Box
-          display={"flex"}
-          flexDirection={"column"}
-          gap={3}
-          justifyContent={"space-between"}
-          sx={{ width: "100%", maxWidth: 400 }}
+            display={"flex"}
+            justifyContent={"center"}
+            alignContent={"center"}
+            minHeight={"100%"}
         >
-          <Grid display={"flex"} flexDirection={"column"}>
-            <Typography
-              display={"flex"}
-              justifyContent={"start"}
-              sx={{
-                mb: 1.5,
-                fontWeight: 500,
-                fontSize: "1.625rem",
-                lineHeight: 1.385,
-              }}
+            <Box
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                minWidth={'100%'}
+                padding={[6, 12]}
             >
-              {`Welcome to ${themeConfig.name}👋`}
-            </Typography>
-            <Typography sx={{ color: "text.secondary" }}>
-              Please sign-in to your account
-            </Typography>
-          </Grid>
-          <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-            <Grid display={"flex"} flexDirection={"column"} gap={2}>
-              <FormControl fullWidth>
-                <Controller
-                  name="email"
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange, onBlur } }) => (
-                    <TextField
-                      autoFocus
-                      label="Email"
-                      value={value}
-                      onBlur={onBlur}
-                      onChange={onChange}
-                      error={Boolean(errors.email)}
-                      placeholder="example@mail.com"
-                    />
-                  )}
-                />
-                {errors.email && (
-                  <FormHelperText sx={{ color: "error.main" }}>
-                    {errors.email.message}
-                  </FormHelperText>
-                )}
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel
-                  htmlFor="auth-login-v2-password"
-                  error={Boolean(errors.password)}
+                <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    gap={3}
+                    justifyContent={"space-between"}
+                    maxWidth={400}
+                    width={'100%'}
                 >
-                  Password
-                </InputLabel>
-                <Controller
-                  name="password"
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange, onBlur } }) => (
-                    <OutlinedInput
-                      value={value}
-                      onBlur={onBlur}
-                      label="Password"
-                      onChange={onChange}
-                      id="auth-login-v2-password"
-                      error={Boolean(errors.password)}
-                      type={showPassword ? "text" : "password"}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            edge="end"
-                            onMouseDown={(e) => e.preventDefault()}
-                            onClick={() => setShowPassword(!showPassword)}
-                          >
-                            {showPassword ? (
-                              <VisibilityOutlined />
-                            ) : (
-                              <VisibilityOffOutlined />
-                            )}
-                            {/* <Icon icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} fontSize={20} /> */}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  )}
-                />
-                {errors.password && (
-                  <FormHelperText sx={{ color: "error.main" }} id="">
-                    {errors.password.message}
-                  </FormHelperText>
-                )}
-              </FormControl>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <FormControlLabel
-                  label="Stay signed in"
-                  control={
-                    <Checkbox
-                      checked={rememberMe}
-                      onChange={(e) => setRememberMe(e.target.checked)}
-                    />
-                  }
-                />
-                <LinkStyled href="/forgot-password">
-                  Forgot Password?
-                </LinkStyled>
-              </Box>
-              <Button size="large" type="submit" variant="contained">
-                Login
-              </Button>
-            </Grid>
-          </form>
+                    <Grid display={"flex"} flexDirection={"column"}>
+                        <Typography
+                            display={"flex"}
+                            justifyContent={"start"}
+                            sx={{
+                                mb: 1.5,
+                                fontWeight: 500,
+                                fontSize: "1.625rem",
+                                lineHeight: 1.385,
+                            }}
+                        >
+                            {`Welcome to ${themeConfig.name}👋`}
+                        </Typography>
+                        <Typography sx={{ color: "text.secondary" }}>
+                            Please sign-in to your account
+                        </Typography>
+                    </Grid>
+                    <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                        <Grid display={"flex"} flexDirection={"column"} gap={2}>
+                            <FormControl fullWidth>
+                                <Controller
+                                    name="email"
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field: { value, onChange, onBlur } }) => (
+                                        <TextField
+                                            autoFocus
+                                            label="Email"
+                                            value={value}
+                                            onBlur={onBlur}
+                                            onChange={onChange}
+                                            error={Boolean(errors.email)}
+                                            placeholder="example@mail.com"
+                                        />
+                                    )}
+                                />
+                                {errors.email && (
+                                    <FormHelperText sx={{ color: "error.main" }}>
+                                        {errors.email.message}
+                                    </FormHelperText>
+                                )}
+                            </FormControl>
+                            <FormControl fullWidth>
+                                <InputLabel
+                                    htmlFor="auth-login-v2-password"
+                                    error={Boolean(errors.password)}
+                                >
+                                    Password
+                                </InputLabel>
+                                <Controller
+                                    name="password"
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field: { value, onChange, onBlur } }) => (
+                                        <OutlinedInput
+                                            value={value}
+                                            onBlur={onBlur}
+                                            label="Password"
+                                            onChange={onChange}
+                                            id="auth-login-v2-password"
+                                            error={Boolean(errors.password)}
+                                            type={showPassword ? "text" : "password"}
+                                            endAdornment={
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        edge="end"
+                                                        onMouseDown={(e) => e.preventDefault()}
+                                                        onClick={() => setShowPassword(!showPassword)}
+                                                    >
+                                                        {showPassword ? (
+                                                            <VisibilityOutlined />
+                                                        ) : (
+                                                            <VisibilityOffOutlined />
+                                                        )}
+                                                        {/* <Icon icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} fontSize={20} /> */}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            }
+                                        />
+                                    )}
+                                />
+                                {errors.password && (
+                                    <FormHelperText sx={{ color: "error.main" }} id="">
+                                        {errors.password.message}
+                                    </FormHelperText>
+                                )}
+                            </FormControl>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    flexWrap: "wrap",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                }}
+                            >
+                                <FormControlLabel
+                                    label="Stay signed in"
+                                    control={
+                                        <Checkbox
+                                            checked={rememberMe}
+                                            onChange={(e) => setRememberMe(e.target.checked)}
+                                        />
+                                    }
+                                />
+                                <LinkStyled href="/forgot-password">
+                                    Forgot Password?
+                                </LinkStyled>
+                            </Box>
+                            <Button size="large" type="submit" variant="contained">
+                                Login
+                            </Button>
+                        </Grid>
+                    </form>
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 LoginPage.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>;
