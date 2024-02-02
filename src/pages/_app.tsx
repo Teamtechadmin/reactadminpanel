45bd74cc0d6f8d8ab79e12fba66e2d7cae557329
akanandhu <akananthu11@gmail.com>
2024-02-02 12:44:02 +0530

    chore: set font to Roboto as default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
+import FontWrapper from "@/components/wrappers/FontWrapper";
 import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto } from 'next/font/google'
 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <FontWrapper>
+      <AuthProvider>
+        <Component  {...pageProps} />
+      </AuthProvider>
+    </FontWrapper>
   );
 }
