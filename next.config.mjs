5f7ca6ea5f8e0c1d8fdb892a2d00ea7484884bb6
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-07-31 11:56:47 +0530

    chore: test webpack configs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ["bucketkeracars.s3.ap-south-1.amazonaws.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.filename = "static/chunks/[name].js";
+      config.output.chunkFilename = "static/chunks/[name].js";
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
